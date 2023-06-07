53e63963c8f23035f7e5070444ae09a011943ea6
Tripo-5 <mcdonaldluke2@gmail.com>
2023-06-07 16:07:18 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class SettingsFrame(tk.Frame):
+    def __init__(self, root, callback):
+        super().__init__(root, width=600, height=400)
+        self.callback = callback
+        self.pack_propagate(0)
+        self.create_widgets()
+    def create_widgets(self):
+        # TODO: Implement the Settings functionality
+        label = ttk.Label(self, text="This is the Settings Frame")
+        label.pack()
+
+        back_button = ttk.Button(self, text="Back", command=self.back_button_click)
+        back_button.pack()
+
+    def back_button_click(self):
+        self.callback()    
