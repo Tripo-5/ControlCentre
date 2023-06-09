87d585bf8a897a599e75a3159df18d19dacefdc9
Tripo-5 <mcdonaldluke2@gmail.com>
2023-06-09 11:12:37 +0100

    Update crypter.py

--- crypter.py
+++ crypter.py
@@ -18,6 +18,7 @@ class CrypterFrame(tk.Frame):
         self.selected_encryption = tk.StringVar()
         self.input_file_path = tk.StringVar()
         self.output_directory = tk.StringVar()
+        self.output_filename = tk.StringVar()
 
         self.create_widgets()
 
@@ -34,6 +35,9 @@ class CrypterFrame(tk.Frame):
         self.output_directory_entry = ttk.Entry(self, textvariable=self.output_directory)
         self.output_directory_button = ttk.Button(self, text="Browse", command=self.browse_output_directory)
 
+        self.output_filename_label = ttk.Label(self, text="Output Filename:")
+        self.output_filename_entry = ttk.Entry(self, textvariable=self.output_filename)
+
         self.builder_button = ttk.Button(self, text="Build", command=self.build_exe)
 
         self.back_button = ttk.Button(self, text="Back", command=self.back_callback)
@@ -46,6 +50,8 @@ class CrypterFrame(tk.Frame):
         self.output_directory_label.pack()
         self.output_directory_entry.pack()
         self.output_directory_button.pack()
+        self.output_filename_label.pack()
+        self.output_filename_entry.pack()
         self.builder_button.pack()
         self.back_button.pack()
 
@@ -63,32 +69,32 @@ class CrypterFrame(tk.Frame):
         encryption_method = self.selected_encryption.get()
         input_file = self.input_file_path.get()
         output_directory = self.output_directory.get()
+        output_filename = self.output_filename.get()
 
-        if not input_file or not output_directory:
-            messagebox.showerror("Error", "Please select an input file and output directory.")
+        if not input_file or not output_directory or not output_filename:
+            messagebox.showerror("Error", "Please provide the input file, output directory, and output filename.")
             return
 
         if encryption_method == "XOR":
-            self.build_xored_exe(input_file, output_directory)
+            self.build_xored_exe(input_file, output_directory, output_filename)
         elif encryption_method == "AES":
-            self.build_aes_exe(input_file, output_directory)
+            self.build_aes_exe(input_file, output_directory, output_filename)
         elif encryption_method == "RSA":
-            self.build_rsa_exe(input_file, output_directory)
+            self.build_rsa_exe(input_file, output_directory, output_filename)
         else:
             messagebox.showerror("Error", "Invalid encryption method selected.")
 
-    def build_xored_exe(self, input_file, output_directory):
-        # Your existing code for generating the builder
+    def build_xored_exe(self, input_file, output_directory, output_filename):
+        # Code for building XOR-encrypted executable
 
-        messagebox.showinfo("Success", "XOR-encrypted builder code generated successfully!")
+        messagebox.showinfo("Success", "XOR-encrypted executable built successfully!")
 
-    def build_aes_exe(self, input_file, output_directory):
-        # Your code for generating the builder with AES encryption
+    def build_aes_exe(self, input_file, output_directory, output_filename):
+        # Code for building AES-encrypted executable
 
-        messagebox.showinfo("Success", "AES-encrypted builder code generated successfully!")
+        messagebox.showinfo("Success", "AES-encrypted executable built successfully!")
 
-    def build_rsa_exe(self, input_file, output_directory):
-        # Your code for generating the builder with RSA encryption
+    def build_rsa_exe(self, input_file, output_directory, output_filename):
+        # Code for building RSA-encrypted executable
 
-        messagebox.showinfo("Success", "RSA-encrypted builder code generated successfully!")
-
+        messagebox.showinfo("Success", "RSA-encrypted executable built successfully!")
