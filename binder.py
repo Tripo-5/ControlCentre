a4ba39000748dfcc51c8c956695bc37de426b27f
Tripo-5 <mcdonaldluke2@gmail.com>
2023-06-09 13:20:54 +0100

    Update binder.py

--- binder.py
+++ binder.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
+import PyInstaller.__main__ as pyinstaller
 
 
 class BinderFrame(tk.Frame):
