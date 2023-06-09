79c6dacbe4db8fcb3c9cbc76d34a5ca20e883a12
Tripo-5 <mcdonaldluke2@gmail.com>
2023-06-09 10:53:37 +0100

    Update builder.py

--- builder.py
+++ builder.py
@@ -4,49 +4,60 @@ import PyInstaller.__main__ as pyinstaller
 
 
 class BuilderFrame(tk.Frame):
-    def __init__(self, root, callback):
-        super().__init__(root, width=600, height=400)
-        self.callback = callback
-        self.pack_propagate(0)
+    def __init__(self, parent, back_callback):
+        super().__init__(parent)
+        self.parent = parent
+        self.back_callback = back_callback
+
+        self.encryption_options = [
+            "XOR",
+            "AES",
+            "RSA"
+        ]
+        self.selected_encryption = tk.StringVar()
+
         self.create_widgets()
 
     def create_widgets(self):
-        # Host option as an IP address entry
-        host_label = ttk.Label(self, text="Host:")
-        host_label.pack()
-        self.host_entry = ttk.Entry(self)
-        self.host_entry.pack()
+        self.encryption_label = ttk.Label(self, text="Encryption Method:")
+        self.encryption_dropdown = ttk.Combobox(self, values=self.encryption_options, textvariable=self.selected_encryption)
+        self.encryption_dropdown.set(self.encryption_options[0])
 
-        # Port option
-        port_label = ttk.Label(self, text="Port:")
-        port_label.pack()
-        self.port_entry = ttk.Entry(self)
-        self.port_entry.pack()
+        self.builder_button = ttk.Button(self, text="Build", command=self.build_exe)
 
-        # Encryption key option
-        key_label = ttk.Label(self, text="Encryption Key:")
-        key_label.pack()
-        self.key_entry = ttk.Entry(self)
-        self.key_entry.pack()
+        self.back_button = ttk.Button(self, text="Back", command=self.back_callback)
 
-        # Build button
-        build_button = ttk.Button(self, text="Build", command=self.build_client)
-        build_button.pack()
+        self.encryption_label.pack()
+        self.encryption_dropdown.pack()
+        self.builder_button.pack()
+        self.back_button.pack()
 
-    def build_client(self):
-        host = self.host_entry.get()
-        port = self.port_entry.get()
-        key = self.key_entry.get()
+    def build_exe(self):
+        encryption_method = self.selected_encryption.get()
 
-        # TODO: Use host, port, and key to build the client
-        
-        # Execute PyInstaller command to build the client
-        pyinstaller.run([
-            "--onefile",
-            "--windowed",
-            "--name=Client",
-            "client.py"
-        ])
+        if encryption_method == "XOR":
+            self.build_xored_exe()
+        elif encryption_method == "AES":
+            self.build_aes_exe()
+        elif encryption_method == "RSA":
+            self.build_rsa_exe()
+        else:
+            messagebox.showerror("Error", "Invalid encryption method selected.")
 
-        self.callback()
+    def build_xored_exe(self):
+        # Your existing code for generating the builder
+
+        messagebox.showinfo("Success", "XOR-encrypted builder code generated successfully!")
+
+    def build_aes_exe(self):
+        # Your code for generating the builder with AES encryption
+
+        messagebox.showinfo("Success", "AES-encrypted builder code generated successfully!")
+
+    def build_rsa_exe(self):
+        # Your code for generating the builder with RSA encryption
+
+        messagebox.showinfo("Success", "RSA-encrypted builder code generated successfully!")
+
+
 
